c2f7bf0be0e6f156b107f1fa9d305da35c7a740f
AM <abdm03142014@gmail.com>
2019-08-10 10:20:03 +0530

    New Stuff

--- Networks/principles_of_digital_communications.mm
+++ Networks/principles_of_digital_communications.mm
@@ -2,10 +2,20 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1565411680840" ID="ID_203515974" MODIFIED="1565411693458" TEXT="Principles of Digital Communications">
 <node CREATED="1565411695781" ID="ID_740948657" MODIFIED="1565411816665" POSITION="right" TEXT="Digital Communication: &#xa;Enormous and normally rapidly growing industry, roughly comparable in size to the computer industry"/>
-<node CREATED="1565411825207" ID="ID_1681007436" MODIFIED="1565411947160" POSITION="right" TEXT="Objective: Study those aspects of communication systems unique to those systems. Little focus on hardsare or software (similar to other systems)">
+<node CREATED="1565411825207" ID="ID_1681007436" MODIFIED="1565412065224" POSITION="right" TEXT="Objective: Study those aspects of communication systems unique to those systems. Little focus on hardsare or software (similar to other systems)">
 <node CREATED="1565411934677" ID="ID_302563739" MODIFIED="1565411942520" TEXT="This course is pre-requisite for">
 <node CREATED="1565411920388" ID="ID_179388475" MODIFIED="1565411925966" TEXT="Stochastic process"/>
 <node CREATED="1565411916061" ID="ID_1364417085" MODIFIED="1565411920038" TEXT="Information theory"/>
+</node>
+<node CREATED="1565412185505" ID="ID_1004805018" MODIFIED="1565412191759" TEXT="What is not studied">
+<node CREATED="1565412191760" ID="ID_93392013" MODIFIED="1565412194415" TEXT="Hardware"/>
+<node CREATED="1565412194870" ID="ID_1709693625" MODIFIED="1565412196599" TEXT="Software"/>
+</node>
+<node CREATED="1565412197681" ID="ID_739893688" MODIFIED="1565412200240" TEXT="What we study">
+<node CREATED="1565412200690" ID="ID_1541043357" MODIFIED="1565412204343" TEXT="Architecture principles">
+<node CREATED="1565412321985" ID="ID_794023341" MODIFIED="1565412325148" TEXT="Theory"/>
+<node CREATED="1565412325573" ID="ID_1169082241" MODIFIED="1565412327603" TEXT="Engineering"/>
+</node>
 </node>
 </node>
 </node>
