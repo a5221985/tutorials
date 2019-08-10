da70fa52436f704e909e91b664f888f351161e4e
AM <abdm03142014@gmail.com>
2019-08-10 10:50:02 +0530

    New Stuff

--- Networks/principles_of_digital_communications.mm
+++ Networks/principles_of_digital_communications.mm
@@ -13,8 +13,28 @@
 </node>
 <node CREATED="1565412197681" ID="ID_739893688" MODIFIED="1565412200240" TEXT="What we study">
 <node CREATED="1565412200690" ID="ID_1541043357" MODIFIED="1565412204343" TEXT="Architecture principles">
-<node CREATED="1565412321985" ID="ID_794023341" MODIFIED="1565412325148" TEXT="Theory"/>
-<node CREATED="1565412325573" ID="ID_1169082241" MODIFIED="1565412327603" TEXT="Engineering"/>
+<node CREATED="1565412321985" ID="ID_794023341" MODIFIED="1565412325148" TEXT="Theory">
+<node CREATED="1565412400805" ID="ID_1267264916" MODIFIED="1565412408177" TEXT="Powerful in communication systems"/>
+<node CREATED="1565412417690" ID="ID_135685577" MODIFIED="1565412421877" TEXT="Claude Shannon">
+<node CREATED="1565412422298" ID="ID_1162943651" MODIFIED="1565412428238" TEXT="Information theory inventor">
+<node CREATED="1565412621148" ID="ID_325982698" MODIFIED="1565412635565" TEXT="It is based on simple toy models"/>
+</node>
+</node>
+<node CREATED="1565412765203" ID="ID_437133179" MODIFIED="1565412839587" TEXT="There is a complex relationship between modeling, theory, exercices and engineering/ design">
+<node CREATED="1565412843825" ID="ID_998796766" MODIFIED="1565412858681" TEXT="Use very simple models to understand ideas">
+<node CREATED="1565412859021" ID="ID_1969604049" MODIFIED="1565412888618" TEXT="Generates powerful general theorems plus insights into more complex models and thus reality">
+<font NAME="SansSerif" SIZE="8"/>
+</node>
+</node>
+</node>
+<node CREATED="1565412913112" ID="ID_1631575469" MODIFIED="1565412948908" TEXT="Exercises are aimed at understanding principles"/>
+</node>
+<node CREATED="1565412325573" ID="ID_1169082241" MODIFIED="1565412327603" TEXT="Engineering">
+<node CREATED="1565412963586" ID="ID_129586343" MODIFIED="1565412994322" TEXT="Engineering deals with approximations and judgement calls based on multiple simple models">
+<node CREATED="1565413081964" ID="ID_1733271838" MODIFIED="1565413107263" TEXT="All problems in enginering are toy problems"/>
+<node CREATED="1565413208830" ID="ID_852083495" MODIFIED="1565413230936" TEXT="We need to understand how the whole system works based on our knowledge of toy problems"/>
+</node>
+</node>
 </node>
 </node>
 </node>
