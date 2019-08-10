1c61c3ad36a35ad03e2fc8c201f1be49ac033e1d
AM <abdm03142014@gmail.com>
2019-08-10 11:00:01 +0530

    New Stuff

---
+++ Mathematical_Modelling/computational_science_and_engineering.mm
@@ -0,0 +1,56 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1565413940449" ID="ID_1449019416" MODIFIED="1565414105934" TEXT="Computational Science &amp; Engineering">
+<node CREATED="1565413963073" ID="ID_109058487" MODIFIED="1565414003868" POSITION="right" TEXT="Constructing model">
+<node CREATED="1565414004417" ID="ID_1701557230" MODIFIED="1565414010916" TEXT="Finding the equations"/>
+<node CREATED="1565414011418" ID="ID_304329598" MODIFIED="1565414014764" TEXT="Solve the equations"/>
+</node>
+<node CREATED="1565414152462" ID="ID_760519910" MODIFIED="1565414154519" POSITION="left" TEXT="Topics">
+<node CREATED="1565414154840" ID="ID_659104098" MODIFIED="1565414157206" TEXT="Matrices">
+<node CREATED="1565414162550" ID="ID_1354710762" MODIFIED="1565414167440" TEXT="K4 - 4 x 4">
+<node CREATED="1565414172459" ID="ID_855325903" MODIFIED="1565414214263">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;2&#160;&#160;-1&#160;&#160;&#160;&#160;0
+    </p>
+    <p>
+      -1&#160;&#160;&#160;&#160;2&#160;&#160;-1
+    </p>
+    <p>
+      &#160;&#160;0&#160;&#160;-1&#160;&#160;&#160;&#160;2
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1565414225390" ID="ID_811040755" MODIFIED="1565414228352" TEXT="B3">
+<node CREATED="1565414228878" ID="ID_877057162" MODIFIED="1565414275111">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;1&#160;&#160;-1&#160;&#160;&#160;&#160;0
+    </p>
+    <p>
+      -1&#160;&#160;&#160;&#160;2&#160;&#160;-1
+    </p>
+    <p>
+      &#160;&#160;0&#160;&#160;-1&#160;&#160;&#160;&#160;1
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1565414351558" ID="ID_1291432145" MODIFIED="1565414353585" TEXT="Properties"/>
+</node>
+</node>
+</node>
+</map>
