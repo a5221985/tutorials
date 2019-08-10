eb799851aef4a202b02a1efffee4d091be028e33
AM <abdm03142014@gmail.com>
2019-08-10 10:10:02 +0530

    New Stuff

---
+++ Networks/principles_of_digital_communications.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1565411680840" ID="ID_203515974" MODIFIED="1565411693458" TEXT="Principles of Digital Communications">
+<node CREATED="1565411695781" ID="ID_740948657" MODIFIED="1565411816665" POSITION="right" TEXT="Digital Communication: &#xa;Enormous and normally rapidly growing industry, roughly comparable in size to the computer industry"/>
+<node CREATED="1565411825207" ID="ID_1681007436" MODIFIED="1565411947160" POSITION="right" TEXT="Objective: Study those aspects of communication systems unique to those systems. Little focus on hardsare or software (similar to other systems)">
+<node CREATED="1565411934677" ID="ID_302563739" MODIFIED="1565411942520" TEXT="This course is pre-requisite for">
+<node CREATED="1565411920388" ID="ID_179388475" MODIFIED="1565411925966" TEXT="Stochastic process"/>
+<node CREATED="1565411916061" ID="ID_1364417085" MODIFIED="1565411920038" TEXT="Information theory"/>
+</node>
+</node>
+</node>
+</map>
