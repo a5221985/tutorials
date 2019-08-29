ba419a997ce0b679a9b3daff07d8d1938bd9991e
AM <abdm03142014@gmail.com>
2019-08-29 07:30:03 +0530

    New Stuff

---
+++ Design/engieering_innovation_and_design.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1567002460845" ID="ID_1430185294" MODIFIED="1567002468912" TEXT="Engineering Innovation and Design">
+<node CREATED="1567002489004" ID="ID_971263949" MODIFIED="1567002500804" POSITION="right" TEXT="Instructor Interview: origins of This Course">
+<node CREATED="1567002649380" ID="ID_429958567" MODIFIED="1567002689268" TEXT="Engineering Innovation and Design"/>
+<node CREATED="1567002692052" ID="ID_390414993" MODIFIED="1567002695534" TEXT="Engineering Leadership">
+<node CREATED="1567002800487" ID="ID_1757680534" MODIFIED="1567002802916" TEXT="Decision making"/>
+<node CREATED="1567002805010" ID="ID_523258240" MODIFIED="1567002807403" TEXT="Advocacy"/>
+<node CREATED="1567002807671" ID="ID_1495197024" MODIFIED="1567002813306" TEXT="Cross cultural communication"/>
+</node>
+<node CREATED="1567002695876" ID="ID_504029169" MODIFIED="1567002710687" TEXT="People and Organizations">
+<node CREATED="1567002714213" ID="ID_611111702" MODIFIED="1567002725695" TEXT="How to work effectively in a company"/>
+</node>
+<node CREATED="1567002727460" ID="ID_419266437" MODIFIED="1567002734055" TEXT="Project Engineering">
+<node CREATED="1567002734998" ID="ID_1098086612" MODIFIED="1567002752786" TEXT="Using tools and skills learnt to assemble">
+<node CREATED="1567002754220" ID="ID_1853629474" MODIFIED="1567002757543" TEXT="Electronic systems">
+<node CREATED="1567002772821" ID="ID_1006082865" MODIFIED="1567002777356" TEXT="To run a complicated program"/>
+</node>
+<node CREATED="1567002760436" ID="ID_1402145858" MODIFIED="1567002763718" TEXT="Management systems">
+<node CREATED="1567002767053" ID="ID_1670760574" MODIFIED="1567002771410" TEXT="To run a complicated program"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1567002512450" ID="ID_1294774696" MODIFIED="1567002529181" POSITION="left" TEXT="Instructor Interview: Teaching Design Thinking"/>
+<node CREATED="1567002532220" ID="ID_1857702220" MODIFIED="1567002544045" POSITION="right" TEXT="Instructor Interview: What Happends in Class?"/>
+<node CREATED="1567002545816" ID="ID_1325897435" MODIFIED="1567002556979" POSITION="left" TEXT="Instructor Interview: Online Student Forum"/>
+<node CREATED="1567002558838" ID="ID_210281470" MODIFIED="1567002574199" POSITION="right" TEXT="Instructor Interview: Grading a Design Course"/>
+<node CREATED="1567002578399" ID="ID_1432152691" MODIFIED="1567002593807" POSITION="left" TEXT="10-Step Design Process and Dieter Ram (Sample Lecuture)"/>
+<node CREATED="1567002595485" ID="ID_893455471" MODIFIED="1567002607031" POSITION="right" TEXT="Research and Stakeholder Analysis (Sample Lecture)"/>
+</node>
+</map>
