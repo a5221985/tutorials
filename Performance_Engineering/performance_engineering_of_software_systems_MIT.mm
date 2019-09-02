c4132b19f0f813cc90264c21a306c8e3aea6e983
AM <abdm03142014@gmail.com>
2019-09-02 11:30:02 +0530

    New Stuff

---
+++ Performance_Engineering/performance_engineering_of_software_systems_MIT.mm
@@ -0,0 +1,29 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1567402580696" ID="ID_1781431964" MODIFIED="1567402869851" TEXT="Performance Engineering of Software Systems">
+<node CREATED="1567402878017" ID="ID_623652772" MODIFIED="1567402888659" POSITION="right" TEXT="Lecture 1: Matrix Multiply: A Case Study"/>
+<node CREATED="1567402889721" ID="ID_1361347997" MODIFIED="1567402895165" POSITION="left" TEXT="Lecture 2: Bit Hacks"/>
+<node CREATED="1567402896203" ID="ID_913810876" MODIFIED="1567402912236" POSITION="right" TEXT="Lecture 3: Basic Performance Engineering"/>
+<node CREATED="1567402913106" ID="ID_1551470920" MODIFIED="1567402926423" POSITION="left" TEXT="Lecture 4: Computer Architecture and Performance Engineering"/>
+<node CREATED="1567402931195" ID="ID_641670157" MODIFIED="1567402940850" POSITION="right" TEXT="Industry Mentor (MITPOSSE) Overview"/>
+<node CREATED="1567402941694" ID="ID_996522732" MODIFIED="1567403050208" POSITION="left" TEXT="Lecture 5: Performance Engineering with Profiling Tools"/>
+<node CREATED="1567403051232" ID="ID_296759086" MODIFIED="1567403058145" POSITION="right" TEXT="Lecture 6: C to Assembly"/>
+<node CREATED="1567403058986" ID="ID_350746129" MODIFIED="1567403076238" POSITION="left" TEXT="Lecture 7: Memory Systems and Performance Engineering"/>
+<node CREATED="1567403076941" ID="ID_1564873114" MODIFIED="1567403087516" POSITION="right" TEXT="Lecture 8: Cache-Efficient Algorithms"/>
+<node CREATED="1567403088643" ID="ID_1879959287" MODIFIED="1567403097881" POSITION="left" TEXT="Lecture 9: Cache-Efficient Algorithms II"/>
+<node CREATED="1567403098608" ID="ID_1564012290" MODIFIED="1567403109363" POSITION="right" TEXT="Lecture 10: Dynamic Storage Allocation"/>
+<node CREATED="1567403110147" ID="ID_1457262281" MODIFIED="1567403122688" POSITION="left" TEXT="Lecture 11: What Compilers Can and Cannot Do"/>
+<node CREATED="1567403123377" ID="ID_914714952" MODIFIED="1567403130915" POSITION="right" TEXT="Lecture 12: Multicore Programming"/>
+<node CREATED="1567403131623" ID="ID_130962530" MODIFIED="1567403140162" POSITION="left" TEXT="Lecture 13: Parallelism and Performance"/>
+<node CREATED="1567403140738" ID="ID_502296409" MODIFIED="1567403154771" POSITION="right" TEXT="Lecture 14: Analysis of Multithreaded Algorithms"/>
+<node CREATED="1567403157634" ID="ID_524167238" MODIFIED="1567403166442" POSITION="left" TEXT="Lecture 15: Nondeterministic Programming"/>
+<node CREATED="1567403247084" ID="ID_1543284942" MODIFIED="1567403256229" POSITION="right" TEXT="Lecture 16: Synchronizing without Locks"/>
+<node CREATED="1567403257028" ID="ID_1425318579" MODIFIED="1567403265817" POSITION="left" TEXT="Lecture 17: Performance Issues in Parallelism"/>
+<node CREATED="1567403266403" ID="ID_1758894819" MODIFIED="1567403278856" POSITION="right" TEXT="Lecture 18: Primer on Ray Tracing Techniques"/>
+<node CREATED="1567403282043" ID="ID_685681834" MODIFIED="1567403294774" POSITION="left" TEXT="Lecture 19: How TokuDB Fractal Tree Indexes Work"/>
+<node CREATED="1567403295460" ID="ID_927821344" MODIFIED="1567403302893" POSITION="right" TEXT="Lecture 20: Distributed Systems"/>
+<node CREATED="1567403303546" ID="ID_683497069" MODIFIED="1567403311096" POSITION="left" TEXT="Lecture 21: Quiz 2 Review"/>
+<node CREATED="1567403311731" ID="ID_1945287518" MODIFIED="1567403331721" POSITION="right" TEXT="Lecture 22: A Tale of 10 Bugs: Performance Engineering at VMware"/>
+<node CREATED="1567403332479" ID="ID_126815055" MODIFIED="1567403343191" POSITION="left" TEXT="Lecture 23: Final Competition and Student Feedback"/>
+</node>
+</map>
