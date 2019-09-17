df5967080862ae3b69ac57be363b70b8a488837b
AM <abdm03142014@gmail.com>
2019-09-17 19:50:02 +0530

    New Stuff

--- programming/structure_and_interpretation.mm
+++ programming/structure_and_interpretation.mm
@@ -1,4 +1,13 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1568690372137" ID="ID_1185809753" MODIFIED="1568690382833" TEXT="Structure and Interpretation"/>
+<node CREATED="1568690372137" ID="ID_1185809753" MODIFIED="1568690382833" TEXT="Structure and Interpretation">
+<node CREATED="1568690683287" ID="ID_1176881403" MODIFIED="1568690694095" POSITION="right" TEXT="1. Overview and Introduction to List"/>
+<node CREATED="1568690695904" ID="ID_1019365099" MODIFIED="1568690706000" POSITION="left" TEXT="2. Procedures and Processes; Substitution Model"/>
+<node CREATED="1568690706625" ID="ID_409369167" MODIFIED="1568690712446" POSITION="right" TEXT="2. Higher-order Procedures"/>
+<node CREATED="1568690713527" ID="ID_969368786" MODIFIED="1568690718544" POSITION="left" TEXT="3. Compound Data"/>
+<node CREATED="1568690719110" ID="ID_399175599" MODIFIED="1568690727536" POSITION="right" TEXT="4. Henderson Escher Example"/>
+<node CREATED="1568690728864" ID="ID_761361033" MODIFIED="1568690741516" POSITION="left" TEXT="5. Symbolic Differentiation; Quotation"/>
+<node CREATED="1568690742276" ID="ID_1574193164" MODIFIED="1568690753761" POSITION="right" TEXT="6. Pattern Matching and Rule-based Substitution"/>
+<node CREATED="1568690757670" ID="ID_1570782566" MODIFIED="1568690763608" POSITION="left" TEXT="7. Generic Operators"/>
+</node>
 </map>
