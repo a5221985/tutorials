fb8a9d015e0e1f78b8a6b87328f6e621b42a1375
AM <abdm03142014@gmail.com>
2019-12-21 13:20:02 +0530

    New Stuff

---
+++ mathematics/statistics_and_probability/statistics_khan_academy.mm
@@ -0,0 +1,92 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1576913305760" ID="ID_984813533" MODIFIED="1576913319978" TEXT="Statistics">
+<node CREATED="1576913325659" ID="ID_332636690" MODIFIED="1576913331044" POSITION="right" TEXT="Introduction to Random Variables">
+<node CREATED="1576914134848" ID="ID_113621700" MODIFIED="1576914154137" TEXT="variable can take multiple values but we do not solve for it">
+<node CREATED="1576914163014" ID="ID_1714768373" MODIFIED="1576914165173" TEXT="It is a function"/>
+</node>
+<node CREATED="1576914171956" ID="ID_1359746366" MODIFIED="1576914206985" TEXT="x = {1 if it rains tomorrow, 0 if it does not rain tomorrow">
+<node CREATED="1576914222247" ID="ID_803495954" MODIFIED="1576914238978" TEXT="random variable is function that maps random process to numbers"/>
+</node>
+<node CREATED="1576914273293" ID="ID_359097841" MODIFIED="1576914284541" TEXT="X = Number facing up when I roll a fair"/>
+<node CREATED="1576914287221" ID="ID_698531411" MODIFIED="1576914294340" TEXT="x = {1 if heads, 0 if tails">
+<node CREATED="1576914298693" ID="ID_687691336" MODIFIED="1576914304757" TEXT="Outcomes of an experiment"/>
+</node>
+<node CREATED="1576914343227" ID="ID_957197067" MODIFIED="1576914353129" TEXT="Two types">
+<node CREATED="1576914353382" ID="ID_745543891" MODIFIED="1576914357611" TEXT="Discrete random variables">
+<node CREATED="1576914366031" ID="ID_1973037244" MODIFIED="1576914371648" TEXT="flip of a coin"/>
+<node CREATED="1576914380460" ID="ID_1052516974" MODIFIED="1576914382589" TEXT="yes/no"/>
+</node>
+<node CREATED="1576914357815" ID="ID_1596346214" MODIFIED="1576914362854" TEXT="Continuous random variables">
+<node CREATED="1576914387749" ID="ID_1410905117" MODIFIED="1576914392836" TEXT="Infinite number of outcomes">
+<node CREATED="1576914399917" ID="ID_1027013517" MODIFIED="1576914410636" TEXT="Could be exact amount of rain in inches tomorrow"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1576913332956" ID="ID_1454027017" MODIFIED="1576913374647" POSITION="left" TEXT="Probability density functions | Probability and Statistics "/>
+<node CREATED="1576913377933" ID="ID_1344818394" MODIFIED="1576913384322" POSITION="right" TEXT="Binomial Distribution">
+<node CREATED="1576913390093" ID="ID_727076026" MODIFIED="1576913391547" TEXT="1."/>
+<node CREATED="1576913391861" ID="ID_1515009686" MODIFIED="1576913393353" TEXT="2."/>
+<node CREATED="1576913394658" ID="ID_1773855575" MODIFIED="1576913395714" TEXT="3."/>
+<node CREATED="1576913398388" ID="ID_1947303807" MODIFIED="1576913400055" TEXT="4."/>
+</node>
+<node CREATED="1576913402667" ID="ID_1431853462" MODIFIED="1576913409645" POSITION="left" TEXT="Expected Value: E(X)"/>
+<node CREATED="1576913413137" ID="ID_849692538" MODIFIED="1576913424890" POSITION="right" TEXT="Expected value of binomial distribution "/>
+<node CREATED="1576913428654" ID="ID_1211791289" MODIFIED="1576913433155" POSITION="left" TEXT="Poisson process">
+<node CREATED="1576913433411" ID="ID_1381010161" MODIFIED="1576913434275" TEXT="1."/>
+<node CREATED="1576913434755" ID="ID_1800253402" MODIFIED="1576913435532" TEXT="2."/>
+</node>
+<node CREATED="1576913440577" ID="ID_904131704" MODIFIED="1576913444939" POSITION="right" TEXT="Law of large numbers"/>
+<node CREATED="1576913448114" ID="ID_1331331555" MODIFIED="1576913455124" POSITION="left" TEXT="Normal distribution excel exercise"/>
+<node CREATED="1576913456493" ID="ID_1639881002" MODIFIED="1576913463232" POSITION="right" TEXT="Introduction to the normal distribution"/>
+<node CREATED="1576913464018" ID="ID_571840300" MODIFIED="1576913480684" POSITION="left" TEXT="ck12.org normal distribution problems: Qualitative sense of normal distributions"/>
+<node CREATED="1576913481357" ID="ID_767749149" MODIFIED="1576913493500" POSITION="right" TEXT="ck12.org normal distribution problems: z-score"/>
+<node CREATED="1576913496526" ID="ID_513617165" MODIFIED="1576913514981" POSITION="left" TEXT="ck12.org normal distribution problems: Empirical rule"/>
+<node CREATED="1576913515861" ID="ID_207719383" MODIFIED="1576913530197" POSITION="right" TEXT="k12.org exercise: Standard normal distribution and the empirical"/>
+<node CREATED="1576913531138" ID="ID_462487473" MODIFIED="1576913547045" POSITION="left" TEXT="ck12.org: More empirical rule and z-score practice"/>
+<node CREATED="1576913548820" ID="ID_1030613825" MODIFIED="1576913564660" POSITION="right" TEXT="Central limit theorem | Inferential statistics"/>
+<node CREATED="1576913566252" ID="ID_458471946" MODIFIED="1576913574006" POSITION="left" TEXT="Sampling distribution of the sample mean"/>
+<node CREATED="1576913575236" ID="ID_1556038405" MODIFIED="1576913581924" POSITION="right" TEXT="Sampling distribution of the sample mean 2"/>
+<node CREATED="1576913585638" ID="ID_1593279379" MODIFIED="1576913589624" POSITION="left" TEXT="Standard error of the mean"/>
+<node CREATED="1576913590471" ID="ID_1646207263" MODIFIED="1576913598015" POSITION="right" TEXT="Sampling distribution example problem"/>
+<node CREATED="1576913598639" ID="ID_1316204317" MODIFIED="1576913607860" POSITION="left" TEXT="Mean and variance of Bernoulli distribution example"/>
+<node CREATED="1576913611589" ID="ID_829166238" MODIFIED="1576913621107" POSITION="right" TEXT="Bernoulli distribution mean and variance formulas"/>
+<node CREATED="1576913621909" ID="ID_1318291863" MODIFIED="1576913625480" POSITION="left" TEXT="Margin of error 1"/>
+<node CREATED="1576913626297" ID="ID_1943062181" MODIFIED="1576913630208" POSITION="right" TEXT="Margin of error 2"/>
+<node CREATED="1576913633608" ID="ID_1342356544" MODIFIED="1576913652110" POSITION="left" TEXT="Confidence interval example | Inferential statistics"/>
+<node CREATED="1576913652791" ID="ID_296602494" MODIFIED="1576913660880" POSITION="right" TEXT="Small sample size confidence intervals"/>
+<node CREATED="1576913661886" ID="ID_1077030605" MODIFIED="1576913672311" POSITION="left" TEXT="Hypothesis testing and p-values | Inferential statistics"/>
+<node CREATED="1576913675502" ID="ID_363878928" MODIFIED="1576913685912" POSITION="right" TEXT="On-tailed and two-tailed tests | Inferential statistics"/>
+<node CREATED="1576913686688" ID="ID_1803608345" MODIFIED="1576913699015" POSITION="left" TEXT="Z-statistics vs. T-statistics | Inferential statistics"/>
+<node CREATED="1576913699865" ID="ID_1904519166" MODIFIED="1576913709351" POSITION="right" TEXT="Type 1 errors | Inferential statistics"/>
+<node CREATED="1576913711951" ID="ID_840190475" MODIFIED="1576913723775" POSITION="left" TEXT="Small sample hypothesis test | Inferential statistics"/>
+<node CREATED="1576913726591" ID="ID_1127886168" MODIFIED="1576913736530" POSITION="right" TEXT="T-statistic confidence interval | Inferential statistics"/>
+<node CREATED="1576913737216" ID="ID_1119897409" MODIFIED="1576913747600" POSITION="left" TEXT="Large sample portion hypothesis testing"/>
+<node CREATED="1576913749241" ID="ID_438065752" MODIFIED="1576913756859" POSITION="right" TEXT="Variance of differences of random variables"/>
+<node CREATED="1576913759731" ID="ID_1241362143" MODIFIED="1576913766546" POSITION="left" TEXT="Difference of sample means distribution"/>
+<node CREATED="1576913767442" ID="ID_62839096" MODIFIED="1576913775790" POSITION="right" TEXT="Confidence interval of difference of means"/>
+<node CREATED="1576913776442" ID="ID_1361891734" MODIFIED="1576913786863" POSITION="left" TEXT="Clarification of confidence interval of difference of means"/>
+<node CREATED="1576913790379" ID="ID_1715820938" MODIFIED="1576913801722" POSITION="right" TEXT="Hypothesis test for difference of means"/>
+<node CREATED="1576913802531" ID="ID_1500744177" MODIFIED="1576913815488" POSITION="left" TEXT="Comparing population proportions 1"/>
+<node CREATED="1576913816396" ID="ID_563102108" MODIFIED="1576913823481" POSITION="right" TEXT="Comparing population proportions 2"/>
+<node CREATED="1576913827173" ID="ID_1388544355" MODIFIED="1576913836746" POSITION="left" TEXT="Hypothesis tes comparing population proportions"/>
+<node CREATED="1576913837450" ID="ID_714372317" MODIFIED="1576913847709" POSITION="right" TEXT="Squared error of regression line | Regression"/>
+<node CREATED="1576913848497" ID="ID_132850256" MODIFIED="1576913862964" POSITION="left" TEXT="Proof (part 1) minimizing squared error to regression line"/>
+<node CREATED="1576913868922" ID="ID_1073424006" MODIFIED="1576913897998" POSITION="right" TEXT="Proof (part 3) minimizing squared error to regression line"/>
+<node CREATED="1576913901326" ID="ID_723689004" MODIFIED="1576913914787" POSITION="left" TEXT="Proof (part 4) minimizing squared error to regression line"/>
+<node CREATED="1576913918575" ID="ID_551720373" MODIFIED="1576913922900" POSITION="right" TEXT="Regression line example"/>
+<node CREATED="1576913926083" ID="ID_126950070" MODIFIED="1576913938510" POSITION="left" TEXT="Proof (part 2) minimizing squared error to regression line"/>
+<node CREATED="1576913941980" ID="ID_638654717" MODIFIED="1576913953838" POSITION="right" TEXT="R-squared or coefficient of determination"/>
+<node CREATED="1576913956021" ID="ID_1418016354" MODIFIED="1576913961539" POSITION="left" TEXT="Second regression example"/>
+<node CREATED="1576913964350" ID="ID_1391761600" MODIFIED="1576913969881" POSITION="right" TEXT="Calculating R-squared"/>
+<node CREATED="1576913970770" ID="ID_222629834" MODIFIED="1576913981322" POSITION="left" TEXT="Covariance and the regression line"/>
+<node CREATED="1576913985035" ID="ID_314533298" MODIFIED="1576913992339" POSITION="right" TEXT="Chi-square distribution introduction"/>
+<node CREATED="1576913993611" ID="ID_211907118" MODIFIED="1576914002847" POSITION="left" TEXT="Pearson&apos;s chi square test (goodness of fit)"/>
+<node CREATED="1576914003860" ID="ID_894399127" MODIFIED="1576914012726" POSITION="right" TEXT="Contingency table chi-square test"/>
+<node CREATED="1576914019997" ID="ID_61943249" MODIFIED="1576914032151" POSITION="left" TEXT="ANOVA 1: Calculating SST (total sum of squares)"/>
+<node CREATED="1576914033384" ID="ID_1069320668" MODIFIED="1576914048662" POSITION="right" TEXT="ANOVA 2: Calculating SSW and SSW (total sum of squares within and between)"/>
+<node CREATED="1576914049495" ID="ID_1029307228" MODIFIED="1576914057607" POSITION="left" TEXT="ANOVA 3: Hypothesis test with F-statistic"/>
+<node CREATED="1576914058445" ID="ID_640309461" MODIFIED="1576914068245" POSITION="right" TEXT="Correlation and causality | Statistical studies"/>
+</node>
+</map>
